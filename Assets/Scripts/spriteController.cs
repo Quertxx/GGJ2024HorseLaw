85518b0817cbb23ad8fd57f482cdf9f4b5289851
Nico999 <czech.filip2003@gmail.com>
2024-01-27 17:04:53 +0000

    Added Horse Lawyer and Placeholders to UI to test sprite controller

---
+++ Assets/Scripts/spriteController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spriteController : MonoBehaviour
+{
+    Image sprite;
+    Color baseColor = new Vector4(1f,1f,1f,1f);
+    Color highlightColor;
+    Color outOfFocusColor = new Vector4(0.5f,0.5f,0.5f,1f);
+    public Gradient fadeIn; //gray to white
+    public Gradient fadeOut; //white to gray
+    public Gradient completeFadeIn; //alpha 0 to alpha 100
+    public Gradient completeFadeOut; //alpha 100 to alpha 0
+
+    private void Awake()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+    void FadeIn() //used to transition from OutOfFocus to BaseColor
+    {
+        StartCoroutine(FadeInLerp());
+    }
+
+    IEnumerator FadeInLerp()
+    {
+        for (int x = 0; x < 100; x++)
+        {
+            sprite.color = fadeIn.Evaluate((float)x / 100);
+            Debug.Log(sprite.color);
+            yield return new WaitForSeconds(0.02f);
+        }   
+    }
+
+    void FadeOut() //used to transition from BaseColor to OutOfFocus
+    {
+        StartCoroutine(FadeOutLerp());
+    }
+
+    IEnumerator FadeOutLerp()
+    {
+        for (int x = 0; x < 100; x++)
+        {
+            sprite.color = fadeOut.Evaluate((float)x / 100);
+            Debug.Log(sprite.color);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    void FadeInCompletely() //used to fade the sprite in from not being visible
+    {
+        StartCoroutine(FadeInCompletelyLerp());
+    }
+
+    IEnumerator FadeInCompletelyLerp()
+    {
+        for (int x = 0; x < 100; x++)
+        {
+            sprite.color = completeFadeIn.Evaluate((float)x / 100);
+            Debug.Log(sprite.color);
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
+
+    void FadeOutCompletely() //used to fade sprite out to not being visible
+    {
+        StartCoroutine(FadeOutCompletelyLerp());
+    }
+
+    IEnumerator FadeOutCompletelyLerp()
+    {
+        for (int x = 0; x < 100; x++)
+        {
+            sprite.color = completeFadeOut.Evaluate((float)x / 100);
+            Debug.Log(sprite.color);
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
+
+    void Disappear() //used to make a sprite disappear immediately with no fading
+    {
+        sprite.color = new Vector4(0f,0f,0f,0f);
+    }
+
+    void Appear() //used to make a sprite appear immediately with no fading
+    {
+        sprite.color = baseColor;
+    }
+}
