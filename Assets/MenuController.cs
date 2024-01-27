d841ce17d366292406bfe5c8fbcb4820b80bba36
Jack Burrup <62547604+Quertxx@users.noreply.github.com>
2024-01-27 16:23:43 +0000

    Horse Model and Main Menu

---
+++ Assets/MenuController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject MainMenuParent;
+    public GameObject creditsParent;
+    public GameObject controlsParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+    public void credits()
+    {
+        MainMenuParent.SetActive(false);
+        creditsParent.SetActive(true);
+    }
+    public void hideCredits()
+    {
+        MainMenuParent.SetActive(true);
+        creditsParent.SetActive(false);
+    }
+    public void hideControls()
+    {
+        MainMenuParent.SetActive(true);
+        controlsParent.SetActive(false);
+    }
+    public void controls()
+    {
+        MainMenuParent.SetActive(false);
+        controlsParent.SetActive(true);
+    }
+}
