5bf298e3ef259ff104a772882e4f4670d4cbbf98
Jay Burrows <Jay.burrows1@hotmail.co.uk>
2024-01-28 13:23:16 +0000

    crowd and fixes

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -13,11 +13,18 @@ public class playerController : MonoBehaviour
     private Rigidbody playerRB2;
     private Camera cam;
 
+    [Header("make sure its linked")]
     public static playerController instance;
     public GameObject playerCollider;
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("sounds")]
+    [SerializeField] float moveSoundTime;
+    public AudioClip[] moveSounds;
+    private AudioSource playerAudioSource;
+
+    private void Awake()
     {
+
         if (instance == null)
         {
             instance = this;
@@ -26,13 +33,19 @@ public class playerController : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
-
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAudioSource = gameObject.GetComponent<AudioSource>();
         playerRef = gameObject.transform.GetChild(0).gameObject;
         playerRB = gameObject.GetComponent<Rigidbody>();
         //playerRB2 = playerRef.GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         cam = Camera.main;
+
+        InvokeRepeating("SoundShit", moveSoundTime, moveSoundTime);
     }
 
     // Update is called once per frame
@@ -58,5 +71,16 @@ public class playerController : MonoBehaviour
         //transform.Translate(desiredDirecton * speed * Time.deltaTime);
         playerRB.velocity = new Vector3(desiredDirecton.x * speed, playerRB.velocity.y, desiredDirecton.z * speed);
         //playerRB2.velocity = new Vector3(desiredDirecton.x * speed, playerRB2.velocity.y, desiredDirecton.z * speed);
+
+        
+    }
+
+    private void SoundShit()
+    {
+        if (playerRB.velocity.magnitude >= 1)
+        {
+            playerAudioSource.clip = moveSounds[Random.Range(0, moveSounds.Length-1)];
+            playerAudioSource.Play();
+        }
     }
 }
